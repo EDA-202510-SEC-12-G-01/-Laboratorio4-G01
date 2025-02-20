dce4fed7c0925c7e0040ce174d575f40cde5c61f
Asthian-Volt <i.barbosac@uniandes.edu.co>
2025-02-20 16:03:50 -0500

    add

--- DataStructures/Stack/stack.py
+++ DataStructures/Stack/stack.py
@@ -1,40 +1,27 @@
 from DataStructures.List import single_linked_list as sl
 
-
 class EmptyStructureError(Exception):
     pass
-
-
 
 def new_stack():
     return sl.new_list()
 
-
-
 def push(my_stack, element):
-    sl.add_first(my_stack, element)
+    sl.add_last(my_stack, element)
     return my_stack
-
-
 
 def pop(my_stack):
     if sl.is_empty(my_stack):
         raise EmptyStructureError("stack is empty")
-    return sl.remove_first(my_stack)
-
-
+    return sl.remove_last(my_stack)
 
 def is_empty(my_stack):
     return sl.is_empty(my_stack)
-
-
 
 def top(my_stack):
     if sl.is_empty(my_stack):
         raise EmptyStructureError("stack is empty")
     return sl.get_first(my_stack)
-
-
 
 def size(my_stack):
     return sl.size(my_stack)
