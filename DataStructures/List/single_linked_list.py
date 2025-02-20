9dc5b06211546f940e2c83a647260b32eaf3409c
Camilo Eduardo Castro Torres <c.castrot@uniandes.edu.co>
2025-02-20 06:51:24 -0500

    funciones de queue, arreglos de single_linked_list, agregar nombre en README

--- DataStructures/List/single_linked_list.py
+++ DataStructures/List/single_linked_list.py
@@ -5,11 +5,17 @@ def new_list():
         'size': 0
     }
 
+
+
 def is_empty(my_list):
     return my_list["size"] == 0
 
+
+
 def size(my_list):
     return my_list["size"]
+
+
 
 def add_first(my_list, element):
     new_node = {
@@ -20,6 +26,8 @@ def add_first(my_list, element):
     if my_list["last"] is None:
         my_list["last"] = new_node
     my_list["size"] += 1
+
+
 
 def add_last(my_list, element):
     new_node = {
@@ -34,11 +42,15 @@ def add_last(my_list, element):
         my_list["last"] = new_node
     my_list["size"] += 1
     
+    
+    
 def first_element(my_list):
     if is_empty(my_list):
         raise Exception('IndexError: list index out of range')
     else:
         return my_list["first"]["info"]
+    
+    
     
 def last_element(my_list):
     if is_empty(my_list):
@@ -46,10 +58,11 @@ def last_element(my_list):
     else:
         return my_list["last"]["info"]
     
+    
+    
 def get_element(my_list, pos):
     if pos < 1 or pos > my_list["size"]:
         raise Exception('IndexError: list index out of range')
-
     searchpos = 1
     node = my_list["first"]
     while searchpos < pos:
@@ -57,34 +70,25 @@ def get_element(my_list, pos):
         searchpos += 1
     return node["info"]
     
+    
+    
 def delete_element(my_list, pos):
     if pos < 1 or pos > my_list["size"]:
-        raise IndexError("list index out of range")
-
+        raise IndexError("Index out of range")
     if pos == 1:
         my_list["first"] = my_list["first"]["next"]
-        if my_list["first"] is None:
-            my_list["last"] = None
     else:
-        searchpos = 1
-        node = my_list["first"]
-        while searchpos < pos - 1:
-            node = node["next"]
-            searchpos += 1
-
-        if node["next"] == my_list["last"]:
-            my_list["last"] = node
-        
-        node["next"] = node["next"]["next"]
-
+        prev = my_list["first"]
+        for _ in range(pos - 2):
+            prev = prev["next"]
+        prev["next"] = prev["next"]["next"]
     my_list["size"] -= 1
-    return my_list
+
 
     
 def remove_first(my_list):
     if is_empty(my_list):
         raise Exception('IndexError: list index out of range')
-    
     first = my_list["first"]["info"]
     my_list["first"] = my_list["first"]["next"]
     if my_list["first"] is None:
@@ -92,10 +96,11 @@ def remove_first(my_list):
     my_list["size"] -= 1
     return first
 
+
+
 def remove_last(my_list):
     if is_empty(my_list):
         raise Exception('IndexError: list index out of range')
-    
     last = my_list["last"]["info"]
     if my_list["first"] == my_list["last"]:
         my_list["first"] = None
@@ -109,31 +114,24 @@ def remove_last(my_list):
     my_list["size"] -= 1
     return last
 
-def insert_element(my_list, pos, element):
+
+
+def insert_element(my_list, element, pos):
     if pos < 1 or pos > my_list["size"] + 1:
-        raise Exception('IndexError: list index out of range')
-    
-    new_node = {
-        "info": element,
-        "next": None
-    }
-    
+        raise IndexError("Index out of range")
     if pos == 1:
         add_first(my_list, element)
     elif pos == my_list["size"] + 1:
         add_last(my_list, element)
     else:
-        searchpos = 1
-        node = my_list["first"]
-        while searchpos < pos - 1:
-            node = node["next"]
-            searchpos += 1
+        new_node = {"info": element, "next": None}
+        prev = my_list["first"]
+        for _ in range(pos - 2):
+            prev = prev["next"]
+        new_node["next"] = prev["next"]
+        prev["next"] = new_node
+        my_list["size"] += 1
 
-        new_node["next"] = node["next"]
-        node["next"] = new_node
-
-    my_list["size"] += 1
-    return my_list
 
 
 def is_present(my_list, element, cmp_function):
@@ -146,53 +144,49 @@ def is_present(my_list, element, cmp_function):
         count += 1
     return -1
 
+
+
 def change_info(my_list, pos, element):
     if pos < 1 or pos > my_list["size"]:
         raise Exception('IndexError: list index out of range')
-
     node = my_list["first"]
     for _ in range(pos - 1):
         node = node["next"]
-
     node["info"] = element 
     return my_list
 
+
+
 def exchange(my_list, pos1, pos2):
-    if pos1 < 1 or pos1 > my_list["size"] or pos2 < 1 or pos2 > my_list["size"]:
-        raise Exception('IndexError: list index out of range')
-
+    if pos1 < 1 or pos2 < 1 or pos1 > my_list["size"] or pos2 > my_list["size"]:
+        raise IndexError("Index out of range")
     if pos1 == pos2:
-        return my_list
+        return  # No hay nada que intercambiar
     
-    if pos1 > pos2:
-        pos1, pos2 = pos2, pos1
-
-    node1 = my_list["first"]
-    node2 = my_list["first"]
-
+    node1 = node2 = my_list["first"]
+    temp1 = temp2 = None
+    
     for i in range(1, max(pos1, pos2) + 1):
         if i == pos1:
-            node1 = node2
+            temp1 = node1
         if i == pos2:
-            break
-        node2 = node2["next"]
+            temp2 = node2
+        node1 = node1["next"] if node1 else None
+        node2 = node2["next"] if node2 else None
+    
+    if temp1 and temp2:
+        temp1["info"], temp2["info"] = temp2["info"], temp1["info"]
 
-    node1["info"], node2["info"] = node2["info"], node1["info"]
 
-    return my_list
 
 def sub_list(my_list, start_index, length):
     if start_index < 1 or start_index > my_list["size"] or start_index + length - 1 > my_list["size"]:
         raise IndexError("list index out of range")
-
     sub_list = new_list()
     temp = my_list["first"]
-
     for _ in range(start_index - 1):
         temp = temp["next"]
-
     for _ in range(length):
         add_last(sub_list, temp["info"])
         temp = temp["next"]
-
     return sub_list
---
+++ DataStructures/Queue/queue.py
@@ -0,0 +1,40 @@
+from DataStructures.List import array_list as al
+
+
+
+class EmptyStructureError(Exception):
+    pass
+
+
+
+def new_queue():
+    return al.new_list()
+
+
+
+def enqueue(my_queue, element):
+    return al.add_last(my_queue, element)
+
+
+
+def dequeue(my_queue):
+    if is_empty(my_queue):
+        raise EmpyStructureError("queue is empty")
+    return al.remove_first(my_queue)
+
+
+
+def peek(my_queue):
+    if is_empty(my_queue):
+        raise EmptyStructureError("queue is empty")
+    return al.first_element(my_queue)
+
+
+
+def is_empty(my_queue):
+    return al.is_empty(my_queue)
+
+
+
+def size(my_queue):
+    return al.size(my_queue)
