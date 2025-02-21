080da665ae27f9ba8932f6599496fd181d377f3e
Camilo Eduardo Castro Torres <c.castrot@uniandes.edu.co>
2025-02-20 23:54:22 -0500

    .

--- App/view.py
+++ App/view.py
@@ -74,17 +74,19 @@ def print_tests_results(queue_results, stack_results):
     Imprime los resultados de las pruebas de rendimiento
     """
     print("\nTiempos de ejecución para Cola: \n")
-
     print("Tiempo de ejecución para enqueue:",
           f"{queue_results['enqueue_time']:.3f}", "[ms]")
     print("Tiempo de ejecución para peek:",
           f"{queue_results['peek_time']:.3f}", "[ms]")
     print("Tiempo de ejecución para dequeue:",
           f"{queue_results['dequeue_time']:.3f}", "[ms]")
-
     print("\nTiempos de ejecución para Pila: \n")
-
-    # TODO Imprimir los resultados de las pruebas de rendimiento de la pila
+    print("Tiempo de ejecución para push:",
+          f"{stack_results['push_time']:.3f}", "[ms]")
+    print("Tiempo de ejecución para top:",
+          f"{stack_results['top_time']:.3f}", "[ms]")
+    print("Tiempo de ejecución para pop:",
+          f"{stack_results['pop_time']:.3f}", "[ms]")
 
 
 # Se crea el controlador asociado a la vista
